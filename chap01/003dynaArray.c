212ee463ee221f993f0a05c2182b881043cc74b0
xkzhai-MacbookAir <xkzhai@MacbookAir.com>
2024-08-30 17:08:51 +0800

    add,remove and find

--- chap01/003dynaArray.c
+++ chap01/003dynaArray.c
@@ -57,6 +57,84 @@ intArr addbyIndex(intArr myArr, int elem, int index){
 intArr addFirst(intArr myArr,int elem){
     return addbyIndex(myArr,elem,0);
 }
+// 查找元素，返回元素所在下标
+int findEle(intArr myArr, int ele){
+    for(int i=0;i<myArr.length;i++){
+        if(myArr.data[i]==ele){
+            return i;
+        }
+    }
+    return -1;
+}
+// 删除末尾元素
+intArr removeLast(intArr myArr){
+    if(myArr.length==0){
+        printf("数组为空，无法删除!\n");
+        return myArr;
+    }
+    myArr.length--;
+    return myArr;
+}
+// 删除指定位置的元素
+intArr removebyIndex(intArr myArr, int index){
+    if(myArr.length==0){
+        printf("数组为空，无法删除!\n");
+        return myArr;
+    }
+    if(index<0 || index>=myArr.length){
+        printf("元素位置不合法，无法删除!\n");
+        return myArr;
+    }
+    for(int i=index;i<myArr.length-1;i++){
+        myArr.data[i] = myArr.data[i+1];
+    }
+    myArr.length--;
+    return myArr;
+}
+// 删除指定元素
+intArr removeEle(intArr myArr, int ele){
+    int index = findEle(myArr,ele);
+    if(index==-1){
+        printf("数组不存在该元素，无法删除！\n");
+        return myArr;
+    }
+    return removebyIndex(myArr, index);
+}
+// 删除数组头部元素
+intArr removeFirst(intArr myArr){
+    if(myArr.length==0){
+        printf("数组为空，无法删除!\n");
+        return myArr;
+    }
+    int ele = myArr.data[0];
+    return removeEle(myArr,ele);
+}
+// 修改数组中指定位置的元素
+intArr updateEle(intArr myArr, int index, int elem){
+    if(index<0 || index>=myArr.length){
+        printf("指定位置不合规!\n");
+        return myArr;
+    }
+    myArr.data[index] = elem;
+    return myArr;
+}
+// 判断某元素是否在数组中
+int contains(intArr myArr, int elem){
+    int index = findEle(myArr, elem);
+    if(index==-1){
+        return 0;
+    }
+    return 1;
+}
+
+// 获取某指定位置的元素值
+int getElembyIndex(intArr myArr, int index){
+    if(index<0 || index>=myArr.length){
+        printf("指定位置不合规!\n");
+        exit(0);
+    }
+    return myArr.data[index];
+}
 int main(void){
     
     intArr myArr = initArr();
@@ -67,6 +145,8 @@ int main(void){
         myArr.length++;
     }
     printArr(myArr);
+    // 插入元素测试
+    printf("=====插入元素测试=======\n");
     myArr = addLast(myArr,20);
     printArr(myArr);
     myArr = addbyIndex(myArr,20,-1);
@@ -77,6 +157,25 @@ int main(void){
     printArr(myArr);
     myArr = addFirst(myArr,20);
     printArr(myArr);
-    
+    // 删除元素测试
+    printf("=====删除元素测试=======\n");
+    myArr = removeLast(myArr);
+    printArr(myArr);
+    myArr = removeFirst(myArr);
+    printArr(myArr);
+    myArr = removeEle(myArr,2);
+    printArr(myArr);
+    myArr = removeEle(myArr,100);
+    printArr(myArr);
+    // 修改元素测试
+    printf("=====修改元素测试=======\n");
+    myArr = updateEle(myArr,2,200);
+    myArr = updateEle(myArr,3,300);
+    printArr(myArr);
+    myArr = updateEle(myArr,20,2000);
+    printArr(myArr);
+    // 查询元素
+    printf("%d\n",contains(myArr,200));
+    printf("%d\n",getElembyIndex(myArr,100));
     return 0;
 }
