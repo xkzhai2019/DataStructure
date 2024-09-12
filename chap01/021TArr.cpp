0759c1cef4ca18f90c7d6233ac86192334285d10
xkzhai <xkzhai@REN9000K.com>
2024-09-12 02:23:04 +0000

    CPP dynamic Arr

--- chap01/021TArr.cpp
+++ chap01/021TArr.cpp
@@ -1,77 +1,72 @@
 #include <iostream>
-//#include <cassert>
-//#include <cstdlib>
 using namespace std;
 #define Size 10
-struct intArr{
+template<class T> // 模版头
+class TArr{
     private:
-        int *data; // 使用数组指针，指向一个名为data的长度不定的数组
+        T *data; // 使用数组指针，指向一个名为data的长度不定的数组
         int length;// 记录当前数组的长度
         int size; // 给数组分配的最大存储容量
         void resize(int newSize); // 扩容函数
     public:
         int getLength();
         int getSize();
-        // int getData(int index) throw(const char *); //C++98/C++03
-        int getData(int index) noexcept(false); // C++11及以上
-        bool setData(int index,int elem);//返回值代表是否修改成功
+        T getData(int index) noexcept(false); // C++11及以上
+        bool setData(int index,T elem);//返回值代表是否修改成功
         void print();
         
         //带参构造函数
-        intArr(int size);
+        TArr(int size);
         // 无参构造函数
-        intArr();
-        void addLast(int elem);
-        void addbyIndex(int index,int elem);
-        void addFirst(int elem);
-        int removeLast() noexcept(false); // 一般删除时，要返回删除的值，因此有可能抛异常
-        int removebyIndex(int index) noexcept(false);
-        int removeFirst() noexcept(false);                
-        int findElem(int elem); // 查找元素，返回下标
-        bool contains(int elem); // 判断数组中是否有该元素
+        TArr();
+        void addLast(T elem);
+        void addbyIndex(int index,T elem);
+        void addFirst(T elem);
+        T removeLast() noexcept(false); // 一般删除时，要返回删除的值，因此有可能抛异常
+        T removebyIndex(int index) noexcept(false);
+        T removeFirst() noexcept(false);                
+        int findElem(T elem); // 查找元素，返回下标
+        bool contains(T elem); // 判断数组中是否有该元素
 };
 
-intArr::intArr(){
-    data = new int[Size];
+template<class T>// 模版头
+TArr<T>::TArr(){
+    data = new T[Size];
     length = 0;
     size = Size;
 }
-intArr::intArr(int size){
-    data = new int[size];
+template<class T>// 模版头
+TArr<T>::TArr(int size){
+    data = new T[size];
     length = 0;
     this->size = size;
 }
-void intArr::print(){
+template<class T>// 模版头
+void TArr<T>::print(){
     cout<<"当前动态数组的最大容量为"<<size<<"，现有元素为："<<endl;
     for(int i=0;i<length;i++){
         cout<<data[i]<<" ";   
     }
     cout<<endl;
 }
-int intArr::getSize(){
+template<class T>// 模版头
+int TArr<T>::getSize(){
     return size;
 }
-int intArr::getLength(){
+template<class T>// 模版头
+int TArr<T>::getLength(){
     return length;
 }
-//int intArr::getData(int index) throw(const char*){
-int intArr::getData(int index) noexcept(false){
-    /*
-    if(index<0 || index>=length){
-        cout<<"索引非法，无法获取元素"<<endl;
-        exit(0); //会直接终止程序
-    }
-    */
-    // 断言,同样会终止程序
-    //assert(index>=0);
-    //assert(index<length);
+template<class T>// 模版头
+T TArr<T>::getData(int index) noexcept(false){
     if(index<0 || index>=length){
         cout<<"索引非法，无法获取值：";
         throw "illegal index error";
     }
     return data[index];
 }
-bool intArr::setData(int index,int elem){
+template<class T>// 模版头
+bool TArr<T>::setData(int index,T elem){
     if(index<0 || index>=length){
         cout<<"索引不合法，无法修改"<<endl;
         return false;
@@ -81,30 +76,25 @@ bool intArr::setData(int index,int elem){
     return true;
 }
 
-void intArr::resize(int newSize){
-    int *newData = new int[newSize];
+template<class T>// 模版头
+void TArr<T>::resize(int newSize){
+    T *newData = new T[newSize];
     for(int i=0;i<length;i++){
         newData[i] = data[i];
     }
     data = newData;
 }
-void intArr::addLast(int elem){
+template<class T>// 模版头
+void TArr<T>::addLast(T elem){
     if(length==size){// 数组已满
-        /*
-        size = 2*size;
-        int *newData = new int[size];
-        for(int i=0;i<length;i++){
-            newData[i] = data[i];
-        }
-        data = newData;
-        */
         size = 2 * size;
         resize(size);
     }
     data[length] = elem;
     length++;
 }
-void intArr::addbyIndex(int index,int elem){
+template<class T>// 模版头
+void TArr<T>::addbyIndex(int index,T elem){
     if(length==size){
         size = 2*size;
         resize(size);
@@ -119,23 +109,18 @@ void intArr::addbyIndex(int index,int elem){
     data[index] = elem;
     length++;
 }
-void intArr::addFirst(int elem){
+template<class T>// 模版头
+void TArr<T>::addFirst(T elem){
     addbyIndex(0,elem);
 }
 
-int intArr::removeLast() noexcept(false){
-    /*
-    if(length==0){
-        cout<<"当前数组为空，不可删除！"<<endl;
-        exit(0);
-    }
-    */
-    //    assert(length>0);
+template<class T>// 模版头
+T TArr<T>::removeLast() noexcept(false){
     if(length==0){
         cout<<"当前数组为空，不可删除：";
         throw "EmptyArr Error";
     }
-    int ret = data[length-1];
+    T ret = data[length-1];
     length--;
     if(length <= size/4){
         size = size/2;
@@ -144,12 +129,13 @@ int intArr::removeLast() noexcept(false){
     return ret;
 }
 
-int intArr::removebyIndex(int index) noexcept(false){
+template<class T>// 模版头
+T TArr<T>::removebyIndex(int index) noexcept(false){
     if(index<0 || index>=length){
         cout<<"索引非法，不可删除:";
         throw "illegal index error";
     }
-    int ret = data[index];
+    T ret = data[index];
     for(int i=index;i<length-1;i++){
         data[i] = data[i+1];
     }
@@ -161,20 +147,12 @@ int intArr::removebyIndex(int index) noexcept(false){
     return ret;
 }
 
-int intArr::removeFirst() noexcept(false){
+template<class T>// 模版头
+T TArr<T>::removeFirst() noexcept(false){
         return removebyIndex(0);
 }
-/*
-int intArr::removeFirst(){
-        try{
-            int ret = removebyIndex(0);
-        }catch(const char *msg){
-            cout<<msg<<endl;
-        }
-        return ret;
-}
-*/
-int intArr::findElem(int elem){
+template<class T>// 模版头
+int TArr<T>::findElem(T elem){
     if(length==0){
         return -1;
     }
@@ -184,7 +162,8 @@ int intArr::findElem(int elem){
     }
     return -1;
 }
-bool intArr::contains(int elem){
+template<class T>// 模版头
+bool TArr<T>::contains(T elem){
     if(length==0){
         return false;
     }
@@ -196,24 +175,20 @@ bool intArr::contains(int elem){
 }
 
 int main(void){
-    //intArr myArr; // 创建对象
-    //myArr.print();
-    intArr myArr(5);
+    TArr<float> myArr(5);
     myArr.print();
     try{
         cout<<"尝试删除空数组最后一个元素:"<<endl;
-        int ret = myArr.removeLast();
+        float ret = myArr.removeLast();
         cout<<"最后一个元素为"<<ret<<endl;
     }catch(const char* msg){
         cout<<msg<<endl;
     }
     for(int i=0;i<40;i++){
         if(i%2==0)
-            myArr.addLast(i);
-            //myArr.addLast(i+0.1);
+            myArr.addLast(i+0.1);
         else
-            myArr.addFirst(i);
-            //myArr.addFirst(i+0.1);
+            myArr.addFirst(i+0.1);
         myArr.print();
     }
     try{
@@ -229,14 +204,14 @@ int main(void){
     }catch(const char* msg){
         cout<<msg<<endl;
     }
-    cout<<"修改-1处元素为20："<<endl;
-    myArr.setData(-1,20);
-    cout<<"修改1处元素为20："<<endl;
-    myArr.setData(1,20);
+    cout<<"修改-1处元素为20.24："<<endl;
+    myArr.setData(-1,20.24);
+    cout<<"修改1处元素为20.24："<<endl;
+    myArr.setData(1,20.24);
 
     try{
         cout<<"尝试删除最后一个元素:"<<endl;
-        int ret = myArr.removeLast();
+        float ret = myArr.removeLast();
         cout<<"最后一个元素为"<<ret<<endl;
     }catch(const char* msg){
         cout<<msg<<endl;
@@ -244,7 +219,7 @@ int main(void){
     myArr.print();
     try{
         cout<<"尝试删除-1处元素:"<<endl;
-        int ret = myArr.removebyIndex(-1);
+        float ret = myArr.removebyIndex(-1);
         cout<<"-1处元素为"<<ret<<endl;
     }catch(const char* msg){
         cout<<msg<<endl;
@@ -252,14 +227,14 @@ int main(void){
     myArr.print();
     try{
         cout<<"尝试删除头部元素:"<<endl;
-        int ret = myArr.removeFirst();
+        float ret = myArr.removeFirst();
         cout<<"头部元素为"<<ret<<endl;
     }catch(const char* msg){
         cout<<msg<<endl;
     }
     myArr.print();
 
-    cout<<"查找100在数组中的位置："<<myArr.findElem(100)<<endl;
-    cout<<"数组中是否包含元素10："<<myArr.contains(10)<<endl;
+    cout<<"查找100在数组中的位置："<<myArr.findElem(100.24)<<endl;
+    cout<<"数组中是否包含元素10："<<myArr.contains(20.24)<<endl;
 
 }
