28aaff727da7a94f312832c73cbd0abaefdab562
xkzhai-ser <xkzhai@ser.com>
2024-09-18 12:45:25 +0000

    minor modify

--- chap02/002ArrayStack.cpp
+++ chap02/002ArrayStack.cpp
@@ -1,4 +1,18 @@
 
+template<typename T>
+class ArrayStack{
+    private:
+
+    public:
+        ArrayStack();
+        ArrayStack(int Size);
+};
+
+template<typename T>
+ArrayStack<T>::ArrayStack(int Size){
+    
+}
+
 
 int main(void){
     return 0;
