56cb6b4bdc3a13837c9948394b9c510788b41dd0
xkzhai-ser <xkzhai@ser.com>
2024-10-16 04:20:38 +0000

    visit tree

--- chap04/BST2.cpp
+++ chap04/BST2.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 template<typename T>
 class Node{
 public:
@@ -16,9 +18,11 @@ public:
 
 template<typename T>
 class BST{
+private:
     Node<T>* root;
     int size;
 
+public:
     BST(){
         root = nullptr;
         size = 0;
@@ -38,7 +42,7 @@ class BST{
     }
     //将元素e放入以node为根结点的二分搜索树中
     // 返回插入新结点后二分搜索树的根
-    Node* add(Node<T> *node, T e){
+    Node<T>* add(Node<T> *node, T e){
         if(node==nullptr){
             size++;
             return new Node<T>(e);
@@ -51,4 +55,81 @@ class BST{
         }
         return node;
     }
+
+    bool contains(T e){
+        return contains(root,e);
+    }
+
+    // 查找以node为节点的二分搜索树中是否包含元素e
+    bool contains(Node<T> *node,T e){
+        if(node==nullptr){
+            return false;
+        }
+        if(node->e == e){
+            return true;
+        }else if(node->e > e){
+            return contains(node->left,e);
+        }else{
+            return contains(node->right,e);
+        }
+    }
+
+    void preOrder(){
+        preOrder(root);
+    }
+
+    // 前序访问以node为根节点的二分搜索树
+    void preOrder(Node<T> *node){
+        if(node==nullptr) return;
+        std::cout<<node->e<<" ";
+        preOrder(node->left);
+        preOrder(node->right);
+    }
+
+    void inOrder(){
+        inOrder(root);
+    }
+    // 中序访问以node为根节点的二分搜索树
+    void inOrder(Node<T> *node){
+        if(node==nullptr) return;
+        inOrder(node->left);
+        std::cout<<node->e<<" ";
+        inOrder(node->right);
+    }
+    
+    void postOrder(){
+        postOrder(root);
+    }
+    // 后序访问以node为根节点的二分搜索树
+    void postOrder(Node<T> *node){
+        if(node==nullptr) return;
+        inOrder(node->left);
+        inOrder(node->right);
+        std::cout<<node->e<<" ";
+    }
+    
 };
+
+int main(){
+    BST<int> bst;
+    int arr[] = {26,20,15,23,14,18,37,32,43,30};
+    /*
+    for(int i=0;i<sizeof(arr)/sizeof(int);i++){
+        bst.add(arr[i]);
+    }
+    */
+    for(int num: arr){
+        bst.add(num);
+    }
+    std::cout<<"是否包含10："<<bst.contains(10)<<std::endl;
+    std::cout<<"是否包含32："<<bst.contains(32)<<std::endl;
+    std::cout<<"前序遍历：";
+    bst.preOrder();
+    std::cout<<std::endl;
+    std::cout<<"中序遍历：";
+    bst.inOrder();
+    std::cout<<std::endl;
+    std::cout<<"后序遍历：";
+    bst.postOrder();
+    return 0;
+}
